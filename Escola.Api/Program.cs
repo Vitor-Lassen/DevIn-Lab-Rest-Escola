b1ea6ae4567caa39ee1034b77c693d57204fc451
Vitor Lassen <vitor@lassen.com.br>
2022-08-29 21:03:35 -0300

    add swagger

--- Escola.Api/Program.cs
+++ Escola.Api/Program.cs
@@ -1,9 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 app.MapControllers();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        options.RoutePrefix = string.Empty;
+    });
+}
 
 app.Run();

847e80b0799ed1181d663df2089dc7a7bd977c69
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-09-03 13:48:16 -0300

    Feature:Criando Bancos de Dados

---
+++ src/Escola.Infra/DataBase/Mappings/MateriaMap.cs
@@ -0,0 +1,31 @@
+﻿using Escola.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Infra.DataBase.Mappings
+{
+    public class MateriaMap: IEntityTypeConfiguration<Materia>
+    {
+        public void Configure(EntityTypeBuilder<Materia> builder)
+        {
+            builder.ToTable("Materia");
+
+            builder.HasKey(m => m.Id);
+
+            builder.Property(m => m.Id)
+                    .UseIdentityColumn()
+                    .ValueGeneratedOnAdd();
+
+            builder.Property(x => x.Nome)
+                    .HasColumnType("varchar")
+                    .HasMaxLength(80)
+                    .HasColumnName("Nome");
+
+        }
+    }
+}

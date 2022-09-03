44df4f6869909be3f982f276bd7dfec54838166b
Eliane Henriqueta <elianehenriqueta@gmail.com>
2022-09-03 12:30:23 -0300

    Feature: Criacao das Moldes

---
+++ src/Escola.Domain/Models/NotasMateria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escola.Domain.Models
+{
+    public class NotasMateria
+    {
+        public int Id { get; set; }
+        public virtual Materia Materia { get; set; }
+        public int MateriaId { get; set; }
+
+        public virtual Boletim Boletim { get; set; }
+        public int BoletimId { get; set; }
+        public double Nota { get; set; }
+    }
+}
